030a06272e63cc0fc797c920c3fe45a051dd6ebc
Edmilson Carmo (de) Oliveira <ecarmoli@emeal.nttdata.com>
2026-06-12 17:31:50 -0300

    fix: Correct Terraform syntax in step-functions variables.tf (remove semicolons)

--- terraform/modules/step-functions/variables.tf
+++ terraform/modules/step-functions/variables.tf
@@ -1,5 +1,20 @@
-variable "environment" { type = string }
-variable "collect_to_cleanse_job_name" { type = string }
-variable "cleanse_to_consume_job_name" { type = string }
-variable "sns_topic_arn" { type = string }
-variable "tags" { type = map(string); default = {} }
+variable "environment" {
+  type = string
+}
+
+variable "collect_to_cleanse_job_name" {
+  type = string
+}
+
+variable "cleanse_to_consume_job_name" {
+  type = string
+}
+
+variable "sns_topic_arn" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
