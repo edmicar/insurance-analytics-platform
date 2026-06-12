4d9e7e2c8578b2dc24efe23604f500cfa8f4db47
Edmilson Carmo (de) Oliveira <ecarmoli@emeal.nttdata.com>
2026-06-12 18:12:44 -0300

    fix: Add DynamoDB table imports (pre-existing from workshop setup)

--- terraform/environments/dev/imports.tf
+++ terraform/environments/dev/imports.tf
@@ -1,11 +1,10 @@
 ###############################################################################
-# Import existing resources created by a previous partial apply
-# These import blocks tell Terraform to adopt already-existing AWS resources
-# into its state, avoiding "AlreadyExists" errors.
-#
-# After successful first apply, this file can be deleted.
+# Import existing resources from the workshop pre-provisioned environment
+# These resources were created by the AWS Workshop setup before Terraform.
+# After first successful apply, this file can be removed.
 ###############################################################################
 
+# S3 Buckets
 import {
   to = module.s3_data_lake.aws_s3_bucket.collect
   id = "dev-insurancelake-169459655914-us-east-1-collect"
@@ -36,6 +35,7 @@ import {
   id = "dev-insurancelake-169459655914-us-east-1-access-logs"
 }
 
+# IAM Roles
 import {
   to = module.glue_etl.aws_iam_role.glue_job_role
   id = "dev-insurancelake-glue-role"
@@ -44,4 +44,25 @@ import {
 import {
   to = module.step_functions.aws_iam_role.step_functions_role
   id = "dev-insurancelake-sfn-role"
+}
+
+# DynamoDB Tables
+import {
+  to = module.dynamodb.aws_dynamodb_table.value_lookup
+  id = "dev-insurancelake-etl-value-lookup"
+}
+
+import {
+  to = module.dynamodb.aws_dynamodb_table.multi_lookup
+  id = "dev-insurancelake-etl-multi-lookup"
+}
+
+import {
+  to = module.dynamodb.aws_dynamodb_table.hash_values
+  id = "dev-insurancelake-etl-hash-values"
+}
+
+import {
+  to = module.dynamodb.aws_dynamodb_table.job_audit
+  id = "dev-insurancelake-etl-job-audit"
 }
